09d04f1777a424b639bd4f8da76613ddce8f157b
TwoTau <TwoTau@users.noreply.github.com>
2016-05-22 01:57:50 -0700

    Add search feature

--- popup.js
+++ popup.js
@@ -2,11 +2,20 @@ $(function() {
 
     var CHAR_LIMIT = 200;
 
-    setup();
+    setup(data);
 
-    function setup() {
-        for(var key in data) {
-            var sectionArray = data[key];
+    $("#search").change(function() {
+        reset();
+        setup(filter($(this).val()));
+    });
+
+    function reset() {
+        $("#all").html("");
+    }
+
+    function setup(dataObj) {
+        for(var key in dataObj) {
+            var sectionArray = dataObj[key];
             var sectionHTML = "<div><h2>" + key + "</h2>";
             for(var i = 0; i < sectionArray.length; i++) {
                 sectionHTML += '<div data-clipboard-text="' + sectionArray[i] + '">' + getSample(sectionArray[i]) + "<span>" + getEnding(sectionArray[i]) + "</span></div>";
@@ -16,7 +25,6 @@ $(function() {
         }
 
         var clipboard = new Clipboard("#all div");
-
     }
 
     function cleanText(text) {
@@ -30,8 +38,21 @@ $(function() {
     function getEnding(longText) {
         return cleanText(longText).substr(CHAR_LIMIT);
     }
-
-    function getRandom() {
-        return copypastas[Math.floor(Math.random()*copypastas.length)];
-    }
 });
+
+function filter(filterKeyword) {
+    filterKeyword = filterKeyword.toLowerCase();
+    var filteredData = $.extend(true, {}, data);
+    for(var key in filteredData) {
+        var sectionArray = filteredData[key];
+        var i = 0;
+        while(i < sectionArray.length) {
+            if(sectionArray[i].toLowerCase().indexOf(filterKeyword) === -1) {
+                sectionArray.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+    }
+    return filteredData;
+}
